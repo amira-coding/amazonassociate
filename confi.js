67964ef2badd8b269d501f130fdc605ff9f485bf
focusing <amiracoding2025@gmail.com>
2025-03-09 03:57:03 -0700

    Initial commit for my new project

---
+++ confi.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#0D6EFD', // Bright Blue
+          secondary: '#00D4FF', // Neon Cyan
+        },
+      },
+    },
+  }
+  
